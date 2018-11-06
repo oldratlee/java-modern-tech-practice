b1e3c75d5b77533fcc284b89c5ced3592e3102da
Jerry Lee <oldratlee@gmail.com>
2018-11-06 20:28:38 +0800

    update CoroutinesTest

--- library/src/test/java/com/example/practice/java/modern/tech/sandbox/library/coro/CoroutinesTest.kt
+++ library/src/test/java/com/example/practice/java/modern/tech/sandbox/library/coro/CoroutinesTest.kt
@@ -1,7 +1,6 @@
 package com.example.practice.java.modern.tech.sandbox.coro
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.Test
 
 class CoroutinesTest {
@@ -25,5 +24,84 @@ class CoroutinesTest {
                 i += 1
             }
         }.forEach(::println)
+    }
+
+
+    @Test
+    fun test1() {
+        runBlocking {
+            val hello: Deferred<String> = async {
+                delay(100)
+                println("hello: [${Thread.currentThread().name}]")
+                hello()
+            }
+//            println("${Date()}: 2")
+//            val world: Deferred<String> = async {
+//                println("world: ${Thread.currentThread().name}")
+//                world()
+//            }
+
+            println("runBlocking [${Thread.currentThread().name}] 1")
+
+            println(hello.await().length)
+            println("runBlocking [${Thread.currentThread().name}] 2")
+
+//            println("${Date()}: 4")
+//            println(world.await().length)
+
+            "Hello"
+        }
+
+    }
+
+    suspend fun hello(): String {
+        delay(1000)
+        return "hello"
+    }
+
+    suspend fun world(): String {
+        delay(1000)
+        return "world"
+    }
+
+    @Test
+    fun eee() {
+        runBlocking<Unit> {
+            launch {
+                // context of the parent, main runBlocking coroutine
+                println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+            }
+            launch(Dispatchers.Unconfined) {
+                // not confined -- will work with main thread
+                println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+            }
+            launch(Dispatchers.Default) {
+                // will get dispatched to DefaultDispatcher
+                println("Default               : I'm working in thread ${Thread.currentThread().name}")
+            }
+            launch(newSingleThreadContext("MyOwnThread")) {
+                // will get its own new thread
+                println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+            }
+        }
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    launch {
+        // context of the parent, main runBlocking coroutine
+        println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Unconfined) {
+        // not confined -- will work with main thread
+        println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Default) {
+        // will get dispatched to DefaultDispatcher
+        println("Default               : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(newSingleThreadContext("MyOwnThread")) {
+        // will get its own new thread
+        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
     }
 }
